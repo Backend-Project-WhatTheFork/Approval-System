929182fee449f2fc205803c00a4da28ac5db4771
chaewookim <chaewookim2877@gmail.com>
2025-12-02 12:37:59 +0900

    [feat] #7 예약 관련 엔티티 구현

---
+++ resource-reservation/src/main/java/com/whatthefork/resourcereservation/domain/CanceledReservation.java
@@ -0,0 +1,35 @@
+package com.whatthefork.resourcereservation.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CanceledReservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;        // resource_management table의 pk 참조하는 fk이자 현 테이블의 pk
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String reason;
+
+    @Column(nullable = false)
+    private LocalDateTime canceledDate;
+
+    @Builder
+    public CanceledReservation(String reason, LocalDateTime canceledDate) {
+        this.reason = reason;
+        this.canceledDate = canceledDate;
+    }
+}
---
+++ resource-reservation/src/main/java/com/whatthefork/resourcereservation/domain/ResourceManagement.java
@@ -0,0 +1,55 @@
+package com.whatthefork.resourcereservation.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ResourceManagement {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long userId;                // user table의 pk 참조하는 fk
+
+    @Column(nullable = false)
+    private Long resourceId;            // resource table의 pk 참조하는 fk
+
+    @Column(nullable = false)
+    private LocalDateTime bookedDate;
+
+    @Column(nullable = false)
+    private LocalDateTime startDate;
+
+    @Column(nullable = false)
+    private LocalDateTime endDate;
+
+    @Column(nullable = false)
+    private int capacity;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String reason;
+
+    @Builder
+    public ResourceManagement(Long userId, Long resourceId, LocalDateTime bookedDate, LocalDateTime startDate, LocalDateTime endDate, int capacity, String reason) {
+        this.userId = userId;
+        this.resourceId = resourceId;
+        this.bookedDate = bookedDate;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.capacity = capacity;
+        this.reason = reason;
+    }
+}
