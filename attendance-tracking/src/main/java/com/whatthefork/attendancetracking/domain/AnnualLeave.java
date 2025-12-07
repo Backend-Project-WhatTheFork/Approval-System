5901b9ff71141b7fd56359a8191473e18adea509
ChoiKYH <priyune@naver.com>
2025-12-07 15:17:15 +0900

    [feat] #20 import 규칙 준수

--- attendance-tracking/src/main/java/com/whatthefork/attendancetracking/domain/AnnualLeave.java
+++ attendance-tracking/src/main/java/com/whatthefork/attendancetracking/domain/AnnualLeave.java
@@ -1,12 +1,17 @@
 package com.whatthefork.attendancetracking.domain;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
-
 import java.time.LocalDateTime;
 
 @Entity
