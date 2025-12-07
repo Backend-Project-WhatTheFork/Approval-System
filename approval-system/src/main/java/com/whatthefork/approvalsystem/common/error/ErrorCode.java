001b9009b002912a73987a7111274629ee5b1411
haengguk <guddnrb14@naver.com>
2025-12-07 19:16:52 +0900

    #38 DocumentService 검증 책임 분리 및 중복 코드 메소드화

--- approval-system/src/main/java/com/whatthefork/approvalsystem/common/error/ErrorCode.java
+++ approval-system/src/main/java/com/whatthefork/approvalsystem/common/error/ErrorCode.java
@@ -15,6 +15,8 @@ public enum ErrorCode {
     DOCUMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "D001", "해당 문서를 찾을 수 없습니다."),
     CANNOT_MODIFY_DOCUMENT(HttpStatus.BAD_REQUEST, "D002", "임시저장 상태의 문서만 수정할 수 있습니다."),
     CANNOT_DELETE_DOCUMENT(HttpStatus.BAD_REQUEST, "D003", "이미 결재가 진행된 문서는 삭제할 수 없습니다."),
+    APPROVER_REQUIRED(HttpStatus.BAD_REQUEST, "D004", "결재자를 지정해야 합니다."),
+    INVALID_APPROVER_COUNT(HttpStatus.BAD_REQUEST, "D005", "결재자는 반드시 3명이어야 합니다."),
 
     // Auth
     NOT_DRAFTER(HttpStatus.FORBIDDEN, "A001", "본인의 문서만 수정/삭제할 수 있습니다.");
