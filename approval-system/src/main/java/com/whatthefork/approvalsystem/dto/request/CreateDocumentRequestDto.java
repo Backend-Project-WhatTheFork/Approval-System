29fcd4b24f01d2dab78b87de4f415da0883de1dc
haengguk <guddnrb14@naver.com>
2025-12-08 20:15:53 +0900

    wip : 게이트웨이 임시 저장

---
+++ approval-system/src/main/java/com/whatthefork/approvalsystem/dto/request/CreateDocumentRequestDto.java
@@ -0,0 +1,35 @@
+package com.whatthefork.approvalsystem.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Setter
+@Getter
+public class CreateDocumentRequestDto {
+
+    @NotBlank(message = "제목은 필수로 입력해야 합니다.")
+    private String title;
+
+    @NotBlank(message = "내용은 필수로 입력해야 합니다.")
+    private String content;
+
+    @NotNull(message = "결재선 목록은 필수입니다.")
+    @Size(min = 3, max = 3, message = "결재선은 필수로 3명을 지정해야 합니다.")
+    private List<Long> approverIds;
+
+    // 참조자, 선택사항
+    private List<Long> referrer;
+
+    @NotNull(message = "휴가 시작 일자는 필수로 지정해야 합니다.")
+    private LocalDate startVacationDate;
+
+    @NotNull(message = "휴가 종료 일자는 필수로 지정해야 합니다.")
+    private LocalDate endVacationDate;
+
+}
