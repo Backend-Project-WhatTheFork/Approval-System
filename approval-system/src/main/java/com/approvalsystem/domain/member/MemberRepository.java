45f634fc02ccc5cb95f2a8bd65cc817d470f8171
haengguk <guddnrb14@naver.com>
2025-12-08 17:04:03 +0900

    wip : 작업 중 저장

---
+++ approval-system/src/main/java/com/approvalsystem/domain/member/MemberRepository.java
@@ -0,0 +1,12 @@
+package com.approvalsystem.domain.member;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface MemberRepository extends JpaRepository<Member,Long> {
+
+    String findByName(String name);
+
+    long countAllByIdIn(List<Long> approvalIds);
+}
