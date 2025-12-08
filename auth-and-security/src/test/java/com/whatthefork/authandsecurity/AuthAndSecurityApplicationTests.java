3761f51e1da9e4a0f3d7c544a665af67dbd54121
ehsy25 <eyoun17@gmail.com>
2025-12-08 15:38:21 +0900

    [refac] moved files to new dev branch for msa style
    # Conflicts:
    #	settings.gradle

---
+++ auth-and-security/src/test/java/com/whatthefork/authandsecurity/AuthAndSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whatthefork.authandsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthAndSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
