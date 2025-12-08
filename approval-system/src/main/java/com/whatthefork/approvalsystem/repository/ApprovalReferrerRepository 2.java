d9d7f30b6b42e5465d37b6f6b4f888c37a333eac
haengguk <guddnrb14@naver.com>
2025-12-08 11:33:01 +0900

    중복 파일 삭제

--- approval-system/src/main/java/com/whatthefork/approvalsystem/repository/ApprovalReferrerRepository 2.java
+++
@@ -1,7 +0,0 @@
-package com.whatthefork.approvalsystem.repository;
-
-import com.whatthefork.approvalsystem.domain.ApprovalReferrer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ApprovalReferrerRepository extends JpaRepository<ApprovalReferrer, Long> {
-}
