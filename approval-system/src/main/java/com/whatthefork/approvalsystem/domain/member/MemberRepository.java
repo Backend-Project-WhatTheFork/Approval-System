5a56818d17c68e481fbba0c72210c926f51723a4
ChoiKYH <priyune@naver.com>
2025-12-08 19:36:17 +0900

    [refactor] moved files to new dev branch for msa style

--- approval-system/src/main/java/com/whatthefork/approvalsystem/domain/member/MemberRepository.java
+++
@@ -1,8 +0,0 @@
-package com.whatthefork.approvalsystem.domain.member;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member,Long> {
-
-    String findByName(String name);
-}
--- approval-system/src/main/java/com/whatthefork/approvalsystem/repository/ApprovalHistoryRepositoy.java
+++
@@ -1,8 +0,0 @@
-package com.whatthefork.approvalsystem.repository;
-
-import com.whatthefork.approvalsystem.domain.ApprovalHistory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ApprovalHistoryRepositoy extends JpaRepository<ApprovalHistory, Long> {
-
-}
