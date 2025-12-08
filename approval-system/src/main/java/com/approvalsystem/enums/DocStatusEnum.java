dc82f3e0434a662469fe7377c69ef5883493ee95
haengguk <guddnrb14@naver.com>
2025-12-08 19:08:27 +0900

    wip : 중복 메소드 메소드화 및 테스트용 memberId 삽입

--- approval-system/src/main/java/com/approvalsystem/enums/DocStatusEnum.java
+++ approval-system/src/main/java/com/approvalsystem/enums/DocStatusEnum.java
@@ -9,7 +9,7 @@ public enum DocStatusEnum {
 
     TEMP("임시 저장"),
     IN_PROGRESS("결재 진행중"),
-    APPROVED("승인 완료"),
+    APPROVED("최종 승인 완료"),
     REJECTED("반려됨");
 
     private final String description;
