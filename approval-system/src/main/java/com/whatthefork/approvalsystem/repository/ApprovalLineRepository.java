9ffdaf0bc47a46ca5b922b537625ca98acd2e57a
haengguk <guddnrb14@naver.com>
2025-12-09 20:27:57 +0900

    #77 [feat] 결재 대기함, 기결재함, 참조 문서함 기안 페이징 조회 비즈니스 로직 구현

--- approval-system/src/main/java/com/whatthefork/approvalsystem/repository/ApprovalLineRepository.java
+++ approval-system/src/main/java/com/whatthefork/approvalsystem/repository/ApprovalLineRepository.java
@@ -1,6 +1,5 @@
 package com.whatthefork.approvalsystem.repository;
 
-import com.whatthefork.approvalsystem.domain.ApprovalDocument;
 import com.whatthefork.approvalsystem.domain.ApprovalLine;
 import com.whatthefork.approvalsystem.enums.LineStatusEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
