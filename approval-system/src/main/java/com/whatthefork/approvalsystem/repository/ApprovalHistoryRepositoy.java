de17d7975f4f837172e139ab3f32f0dc37dab9fc
haengguk <guddnrb14@naver.com>
2025-12-09 10:11:27 +0900

    #67 상신, 상신 취소 컨트롤러 API 구현

--- approval-system/src/main/java/com/whatthefork/approvalsystem/repository/ApprovalHistoryRepositoy.java
+++ approval-system/src/main/java/com/whatthefork/approvalsystem/repository/ApprovalHistoryRepositoy.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface ApprovalHistoryRepositoy extends JpaRepository<ApprovalHistory, Long> {
 
     ApprovalHistory findApprovalHistoryByDocumentId(Long documentId);
+
     boolean existsByDocumentAndActorAndActionType(Long documentId, Long actor, ActionTypeEnum actionType);
 
 }
