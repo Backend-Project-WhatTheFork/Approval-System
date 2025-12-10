ae1b3bee6b301e65f4d2b66937d58a3489c4bb2c
chaewookim <chaewookim2877@gmail.com>
2025-12-10 09:57:34 +0900

    pull dev

--- user-service/src/main/java/com/whatthefork/userservice/auth/repository/RefreshTokenRepository.java
+++
@@ -1,7 +0,0 @@
-package com.whatthefork.userservice.auth.repository;
-
-import com.whatthefork.userservice.auth.entity.RefreshToken;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RefreshTokenRepository extends JpaRepository<RefreshToken, String> {
-}
