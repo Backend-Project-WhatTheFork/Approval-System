ead71065780344dbd8fe82599599eb75ef30caa3
haengguk <guddnrb14@naver.com>
2025-12-10 10:28:19 +0900

    [chore] 유저 모듈 Swagger 설정 수정

--- user-service/src/main/java/com/whatthefork/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/whatthefork/userservice/UserServiceApplication.java
@@ -1,11 +1,14 @@
 package com.whatthefork.userservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 public class UserServiceApplication {
 
     public static void main(String[] args) {
