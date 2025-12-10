fb80a12d874b8127259917755c40f4fd6eb6cfce
chaewookim <chaewookim2877@gmail.com>
2025-12-10 10:28:21 +0900

    conflict 해결

---
+++ approval-system/src/main/java/com/whatthefork/approvalsystem/common/error/GlobalExceptionHandler.java
@@ -0,0 +1,39 @@
+package com.whatthefork.approvalsystem.common.error;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    /* BusinessException 처리 */
+    @ExceptionHandler(BusinessException.class)
+    public ResponseEntity<ErrorResponse> handleBusinessException(BusinessException ex) {
+        log.error("handleBusinessException 에러 발생", ex);
+        log.error("BusinessException - Code: {}, Message: {}", ex.getErrorCode().getCode(), ex.getMessage());
+
+        ErrorCode errorcode = ex.getErrorCode();
+
+        ErrorResponse response = ErrorResponse.of(errorcode);
+
+        return ResponseEntity
+                .status(errorcode.getStatus())
+                .body(response);
+    }
+
+    /* 그 외 에러 전역 처리 */
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception ex) {
+        log.error("handleException 에러 발생", ex);
+
+        ErrorCode errorCode = ErrorCode.INTERNAL_SERVER_ERROR;
+        ErrorResponse response = ErrorResponse.of(errorCode);
+
+        return ResponseEntity
+                .status(errorCode.getStatus())
+                .body(response);
+    }
+}
