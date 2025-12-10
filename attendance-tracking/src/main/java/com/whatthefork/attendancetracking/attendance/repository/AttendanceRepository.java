c3f20850ce6855519a55ce174aeabc4054dac22b
ChoiKYH <priyune@naver.com>
2025-12-10 14:29:47 +0900

    [feat] errorCode, GlobalExceptionHandler 수정

--- attendance-tracking/src/main/java/com/whatthefork/attendancetracking/attendance/repository/AttendanceRepository.java
+++ attendance-tracking/src/main/java/com/whatthefork/attendancetracking/attendance/repository/AttendanceRepository.java
@@ -25,4 +25,6 @@ public interface AttendanceRepository extends JpaRepository<Attendance, Long> {
     Optional<Attendance> findByUserIdAndPunchInDateBetweenOrderByPunchInDateAsc(Long userId, LocalDateTime startDate, LocalDateTime endDate);
 
     List<Attendance> findAllByUserIdAndPunchInDateBetweenOrderByPunchInDateAsc(Long userId, LocalDateTime startDate, LocalDateTime endDate);
+
+    boolean findAttendanceByUserIdAndPunchOutDate(Long userId, LocalDateTime punchOutDate);
 }
--- attendance-tracking/src/main/java/com/whatthefork/attendancetracking/common/error/ErrorCode.java
+++ attendance-tracking/src/main/java/com/whatthefork/attendancetracking/common/error/ErrorCode.java
@@ -6,6 +6,10 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum ErrorCode {
+    // Common
+    INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "C001", "잘못된 입력값입니다."),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "C002","서버 내부 오류가 발생했습니다."),
+    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "C002", "해당 사용자를 찾을 수 없습니다."),
 
     //
     ATTENDANCE_ALREADY_CHECKED_IN(HttpStatus.BAD_REQUEST, "A001","이미 출근을 찍었습니다."),
